c702ae157133de716af72b343c8c31645fb464e0
Denisa <denisa@ubuntu.(none)>
2012-10-01 23:33:31 +0400

    removed some files

--- db/migrate/20120901195858_add_money_is_moderated_to_advert.rb
+++
@@ -1,6 +0,0 @@
-class AddMoneyIsModeratedToAdvert < ActiveRecord::Migration
-  def change
-    add_column :adverts, :money, :string
-    add_column :adverts, :isModerated, :boolean
-  end
-end
--- db/migrate/20120901200449_add_telefon_to_advert.rb
+++
@@ -1,5 +0,0 @@
-class AddTelefonToAdvert < ActiveRecord::Migration
-  def change
-    add_column :adverts, :telefon, :string
-  end
-end
